202c9e171a3d2a8b3f43603337adf9e77ab69c3f
SalvoBevilacqua <salvatorebevilacqua91@gmail.com>
2023-11-22 10:23:48 +0100

    correction:add_isloading

--- script.js
+++ script.js
@@ -4,19 +4,39 @@ createApp({
     data() {
         return {
             numberOfMail: 10,
-            mailArray: []
+            mailArray: [],
+            mailArrayButton: [],
+            isLoading: false,
+            isLoadingButton: false
         }
     },
     created() {
+        this.isLoading = true;
         for (let i = 0; i < this.numberOfMail; i++) {
             axios
             .get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((resp) => {
                 this.mailArray.push(resp.data.response);
+                if (this.mailArray.length === 10) {
+                    this.isLoading = false;
+                }
             });            
         }
-        console.log(this.mailArray);
     },
-    methods: {        
+    methods: {    
+        generateMail() {
+            this.isLoadingButton = true;
+            this.mailArrayButton.length = 0;
+            for (let i = 0; i < this.numberOfMail; i++) {
+                axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((resp) => {
+                    this.mailArrayButton.push(resp.data.response);
+                    if (this.mailArrayButton.length === 10) {
+                        this.isLoadingButton = false;
+                    }
+                });            
+            }
+        }    
     }
 }).mount("#app");
